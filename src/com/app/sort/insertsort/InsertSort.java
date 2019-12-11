f3c06b13ae750e6b8a65f745e160c89317cc3e6c
Patar.Tambunan <Patar.Tambunan@itpega01.SINARMASMSIGLIFE.CO.ID>
2019-12-11 13:36:57 +0700

    Insertion Sort

---
+++ src/com/app/sort/insertsort/InsertSort.java
@@ -0,0 +1,29 @@
+package com.app.sort.insertsort;
+
+public class InsertSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	
+	public void sort(int arr[]){
+		int n = arr.length; 
+        for (int i = 1; i < n; ++i) { 
+            int key = arr[i]; 
+            int j = i - 1; 
+  
+            /* Move elements of arr[0..i-1], that are 
+               greater than key, to one position ahead 
+               of their current position */
+            while (j >= 0 && arr[j] > key) { 
+                arr[j + 1] = arr[j]; 
+                j = j - 1; 
+            } 
+            arr[j + 1] = key; 
+        } 
+	}
+	
+}
